debf4ea6a285f2319266e5d0b40d16715ecd1cc8
pablobarreiro <pbarreiro92@gmail.com>
2022-11-07 18:56:00 -0300

    add marble and light routes in index

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,15 @@ const userRouter = require("./user");
 const projectsRouter = require('./projects')
 const carpentryRouter = require('./carpentry')
 const ironWorkingRouter = require('./ironWorking')
+const lightRouter = require('./light')
+const marbleRouter = require('./marble')
 
 router.use("/user", userRouter);
 router.use('/projects', projectsRouter)
 router.use('/carpentry', carpentryRouter)
 router.use('/ironWorking', ironWorkingRouter)
+router.use('/light', lightRouter)
+router.use('/marble', marbleRouter)
 
 
 module.exports = router;
