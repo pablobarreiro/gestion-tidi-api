af3ff5c88f2d42b50e9a7eb6a03b1ecb8b6ded40
pablobarreiro <pbarreiro92@gmail.com>
2023-06-28 00:14:55 -0300

    morgan added

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const db = require("./db");
 const routes = require("./routes");
+const morgan = require("morgan")
 require("dotenv").config({ path: ".env" });
 
 const app = express();
 
 const corsOptions = { origin: process.env.CLIENT_BASE_URI };
 app.use(cors(corsOptions));
+app.use(morgan('combined'))
 app.use(express.json());
 app.use(cookieParser());
 
